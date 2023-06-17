abf5d86c7af01a89db6a86a7e854e09e6b64607a
Sol <0seekingsolitude01@gmail.com>
2023-06-17 15:02:04 +0800

    add B - aCcepted

---
+++ ProgVar/aCcepted.cpp
@@ -0,0 +1,27 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main () {
+        string S;
+        std::cin >> S;
+        
+        int L = S.size();
+        string ans = "AC";
+        if (S[0] != 'A') {
+            ans ="WA";
+        }
+        
+        int count = 0;
+        for (int i = 1; i < L; ++i) {
+            if(isupper(S[i])) {
+                if (i == 1 || i == L -1 || S[i] != 'C') {
+                    ans = "WA";
+                }
+                ++count;
+            }
+        }
+        if (count != 1) {
+            ans = "WA";
+        }
+        cout << ans << endl;
+    }
