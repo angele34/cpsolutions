4f7ed91364b1b0acd1e7cf4943eee5f3e90ab203
Sol <90606365+SeekingSolitude@users.noreply.github.com>
2023-07-18 15:46:55 +0800

    add 811 - A

--- ProgVar/811A.cpp
+++ ProgVar/811A.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
+    int a, b;
+    cin >> a >> b;
     
-    // if x > N
+    for (int i = 1; ;i++) {
+        if (i % 2 == 0) {
+            b = b-i;
+            if (b < 0) {
+            cout << "Valera" << endl;
+            break;
+    }
+        }
+        else {
+            a = a - i;
+            if (a < 0) {
+                cout << "Vladik" << endl;
+                break;
+            }
+        }
+    }
+    return 0;
 }
