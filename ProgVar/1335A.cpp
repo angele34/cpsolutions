80990eb0977e33198a73830d2dc7033532400681
Sol <90606365+SeekingSolitude@users.noreply.github.com>
2023-07-28 22:11:35 +0800

    add 1335 A problem

---
+++ ProgVar/1335A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        uint64_t n;
+        cin >> n;
+        uint64_t count = (n - 1) / 2;
+        cout << count << endl;
+    }
+    return 0;
+}
