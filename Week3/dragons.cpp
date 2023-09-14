eed329c82cf846d5f144813407131546fbf6aaae
Sol <90606365+SeekingSolitude@users.noreply.github.com>
2023-09-14 19:53:33 +0800

    add dragons 230A

---
+++ Week3/dragons.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int s,n;
+    cin >> s >> n;
+    
+    vector<pair<int, int>> d;
+    for (int i = 0; i < n; i++) {
+        int x,y;
+        cin >> x >> y;
+        d.push_back({x,y});
+    } 
+    sort(d.begin(), d.end());
+    
+    bool proceed = true;
+    for (const auto& pair : d) {
+        if (s <= pair.first) {
+            proceed = false;
+            break;
+        } else s += pair.second; proceed = true;
+    }
+    cout << (proceed ? "YES\n" : "NO\n");
+    return 0;
+}
