411be60ea245845b007974feef05ce869b6415e4
Sol <90606365+SeekingSolitude@users.noreply.github.com>
2023-09-15 19:33:46 +0800

    add 1850C

---
+++ Week3/WordOnPaperC.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    vector<char> v;
+    while (t--) {
+        char grid[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        string c;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (isalnum(grid[i][j])) {
+                    c+=grid[i][j];
+                }
+            }
+        }
+        cout << c << '\n';
+    }
+    return 0;
+}
