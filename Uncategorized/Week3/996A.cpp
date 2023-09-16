1732b263dcc0089a732099eeeddab0cb808ce408
Sol <90606365+SeekingSolitude@users.noreply.github.com>
2023-09-16 17:52:49 +0800

    add 996A

--- Uncategorized/Week3/996A.cpp
+++ Uncategorized/Week3/996A.cpp
@@ -6,16 +6,14 @@ int main() {
     lli n;
     cin >> n;
 
-    vector<int> v = {1, 5, 10, 20, 100};
-    sort(v.rbegin(), v.rend());
-    
+    vector<int> v = {100, 20, 10, 5, 1};
     lli sum = 0, count = 0;
+    
     for (int i = 0; i < v.size(); i++) {
         while (sum + v[i] <= n) {
             sum += v[i];
             count++;
         }
-        if (sum == n) break;
     }
     cout << count << endl;
     return 0;
