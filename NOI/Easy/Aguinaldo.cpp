773cf4cdad521fe4ed276b387529436468d9236b
Sol <90606365+SeekingSolitude@users.noreply.github.com>
2023-10-01 13:38:35 +0800

    add problem solution

---
+++ NOI/Easy/Aguinaldo.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        lli n,p;
+        cin >> n >> p;
+        cout << "Php" << " " << n*p << '\n';
+    }
+
+    return 0;
+}
